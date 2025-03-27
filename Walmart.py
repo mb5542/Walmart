aa5ceb864121702adc79ae8a486c54110b4d6e11
mb5542 <michal5542@gmail.com>
2025-03-27 19:13:34 +0100

    Data preparation

--- Walmart.py
+++ Walmart.py
@@ -50,3 +50,65 @@ print(walmart.info())
 print('\n')
 
 # 2.
+# Check column content
+print(walmart['Purchase_Amount'].value_counts())
+print('\n')
+# The number is concatenated with the $ symbol so this data type is not numeric
+
+# Delete $ symbol 
+walmart['Purchase_Amount']=walmart['Purchase_Amount'].str.replace('$',"")
+
+# Change data type
+walmart['Purchase_Amount'] = walmart['Purchase_Amount'].astype('float')
+print(walmart['Purchase_Amount'].head())
+print('\n')
+
+# Check column content
+print(walmart['Age'].value_counts())
+print('\n')
+# This column contains inappriopriete value 'West Carolyn'
+
+# pd.set_option('display.max_columns',6)
+print(walmart.loc[walmart['Age']=='West Carolyn'])
+print('\n')
+# The entire row is messed up
+
+#Delete invalid row
+walmart = walmart.loc[walmart['Age']!='West Carolyn']
+print(walmart['Age'].value_counts())
+print('\n')
+
+# Change data type
+walmart['Age'] = walmart['Age'].astype('int')
+print(walmart['Age'].head())
+print('\n')
+
+print(walmart.info())
+print('\n')
+
+# 3.
+# Check column content
+print(walmart['Purchase_Date'].value_counts())
+
+# Change data type
+walmart['Purchase_Date'] = pd.to_datetime(walmart['Purchase_Date'])
+print(walmart['Purchase_Date'].head())
+
+print(walmart.info())
+print('\n')
+
+# 4
+# Check column content and change data type
+print(walmart['Gender'].value_counts())
+walmart['Gender'] = walmart['Gender'].astype('category')
+
+print(walmart['Category'].value_counts())
+walmart['Category'] = walmart['Category'].astype('category')
+
+print(walmart['Product_Name'].value_counts())
+walmart['Product_Name'] = walmart['Product_Name'].astype('category')
+
+print(walmart.info())
+# Gender, Category, Product_Name, Payment_Method can be changed to a 'category' type
+
+
